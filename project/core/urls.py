423b7fa9dda5a70a347ae44cee4e84f210488a3e
francoEmunoz <munozfranco011@gmail.com>
2023-04-07 02:05:56 -0300

    delete blog url

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import home, about, contact, sample, blog, store
+from .views import home, about, contact, sample, store
 
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
     path('sample/', sample, name='sample'),
-    path('blog/', blog, name='blog'),
     path('store/', store, name='store')
 ]
