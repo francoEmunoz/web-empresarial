6ccfcda9da87f61b42879e0c3e1d755c23308160
francoEmunoz <munozfranco011@gmail.com>
2023-04-11 23:07:58 -0300

    delete sample url

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import home, about, contact, sample, store
+from .views import home, about, contact, store
 
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
-    path('sample/', sample, name='sample'),
     path('store/', store, name='store')
 ]
