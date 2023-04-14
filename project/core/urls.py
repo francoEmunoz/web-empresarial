671d08acc8681b4d6fbf19e1a3a8a9b5b773509c
francoEmunoz <munozfranco011@gmail.com>
2023-04-13 21:43:20 -0300

    deleted contact url

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import home, about, contact, store
+from .views import home, about, store
 
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
-    path('contact/', contact, name='contact'),
     path('store/', store, name='store')
 ]
